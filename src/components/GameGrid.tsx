4daba8269377e821fcfbd34c6b9a1350c930d322
Kathy <Kathy.wazowski@gmail.com>
2024-12-02 18:23:52 +0800

    Add infinite scroll

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,64 +1,56 @@
-import { Button, HStack, SimpleGrid } from "@chakra-ui/react";
+import { SimpleGrid, Spinner } from "@chakra-ui/react";
+import React from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { GameQuery } from "../App";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardContainer from "./GameCardContainer";
 import GameCardSkeleton from "./GameCardSkeleton";
-import React from "react";
 
 interface Props {
   gameQuery: GameQuery;
 }
 
 const GameGrid = ({ gameQuery }: Props) => {
-  const {
-    data,
-    error,
-    isLoading,
-    fetchNextPage,
-    hasNextPage,
-    isFetchingNextPage,
-  } = useGames(gameQuery);
+  const { data, error, isLoading, fetchNextPage, hasNextPage } =
+    useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
-  console.log(data, "games");
-
+  const fetchedGamesCount =
+    data?.pages.reduce((prev, current) => prev + current.results.length, 0) ||
+    0;
+  console.log(data?.pages, "pages", fetchedGamesCount, "count");
   return (
     <>
       {error && <p>{error.message}</p>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        spacing={6}
-        paddingY={4}
+      <InfiniteScroll
+        dataLength={fetchedGamesCount} //这是已有data的数量
+        next={fetchNextPage}
+        hasMore={!!hasNextPage}
+        loader={<Spinner />}
       >
-        {isLoading &&
-          skeletons.map((skeleton) => (
-            <GameCardContainer key={skeleton}>
-              <GameCardSkeleton />
-            </GameCardContainer>
-          ))}
-        {/* infiniteQuery传回来的数据是.pages里包含每一页的数据 */}
-        {data?.pages.map((page, index) => (
-          <React.Fragment key={index}>
-            {page.results.map((game) => (
-              <GameCardContainer key={game.id}>
-                <GameCard game={game} />
+        <SimpleGrid
+          columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+          spacing={6}
+          paddingY={4}
+        >
+          {isLoading &&
+            skeletons.map((skeleton) => (
+              <GameCardContainer key={skeleton}>
+                <GameCardSkeleton />
               </GameCardContainer>
             ))}
-          </React.Fragment>
-        ))}
-
-        {hasNextPage && (
-          <HStack>
-            <Button
-              onClick={() => fetchNextPage()}
-              isLoading={isFetchingNextPage}
-              isDisabled={!hasNextPage}
-            >
-              {isFetchingNextPage ? "Loading..." : "Load More"}
-            </Button>
-          </HStack>
-        )}
-      </SimpleGrid>
+          {/* infiniteQuery传回来的数据是.pages里包含每一页的数据 */}
+          {data?.pages.map((page, index) => (
+            <React.Fragment key={index}>
+              {page.results.map((game) => (
+                <GameCardContainer key={game.id}>
+                  <GameCard game={game} />
+                </GameCardContainer>
+              ))}
+            </React.Fragment>
+          ))}
+        </SimpleGrid>
+      </InfiniteScroll>
     </>
   );
 };
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -31,7 +31,7 @@ const useGames = (gameQuery: GameQuery) => {
     getNextPageParam: (lastPage, allPages) => {
       return lastPage.next ? allPages.length + 1 : undefined;
     },
-    staleTime: 24 * 60 * 60 * 1000,
+    staleTime: 24 * 60 * 60 * 1000, // 24h
   });
 };
 
